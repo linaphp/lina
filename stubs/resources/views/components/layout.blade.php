639e14844623fd858251e0b6d83860229af6ca31
bangnokia <bangnokia@gmail.com>
2020-11-13 00:25:48 +0700

    add tailwindscs and alpinejs by default

--- stubs/resources/views/components/layout.blade.php
+++ stubs/resources/views/components/layout.blade.php
@@ -2,6 +2,9 @@
 <html>
 <head>
 	<title>{{ $title ?? 'Home page' }}</title>
+
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 </head>
 <body>
 	{{ $slot }}
