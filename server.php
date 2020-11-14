47a85015a487901a6c2c51db2b6264ab47650fd0
bangnokia <bangnokia@gmail.com>
2020-11-15 01:33:26 +0700

    work in progress

--- server.php
+++ server.php
@@ -5,15 +5,8 @@ use App\Builder;
 
 require __DIR__.'/vendor/autoload.php';
 
-// I have no idea about these code
-// should investigate later
 $app = require_once __DIR__.'/bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-$kernel->handle(
-    $input = new Symfony\Component\Console\Input\ArgvInput,
-    new Symfony\Component\Console\Output\ConsoleOutput
-);
-// end
+$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
 
 $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
@@ -26,7 +19,7 @@ $posts = collect($builder->storage()->files('posts'))->map(fn($filePath) => $bui
 if (preg_match('/^\/posts\/(.+)\.html$/', $uri, $matches) === 1) {
     $slug = $matches[1];
     $post = $posts->first(fn($post) => $post->slug === $slug);
-    echo view($post->layout, ['post' => $post])->render(); // the fuck
+    echo view($post->layout, ['post' => $post])->render();
 } elseif (preg_match('/^\/([a-zA-Z_0-9\-]+)\.html$/', $uri, $matches) === 1) {
     echo view('pages.'.$matches[1], ['posts' => $posts])->render();
 } else {
