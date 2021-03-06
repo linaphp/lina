d396f3f455c274c647176db7df035cf8aed47ba1
bangnokia <bangnokia@gmail.com>
2021-03-07 00:33:52 +0700

    remove tags stubs

--- stubs/resources/views/pages/index.blade.php
+++ stubs/resources/views/pages/index.blade.php
@@ -4,7 +4,6 @@
 			<li class="mb-3">
 				<div class="text-sm">
 					<time class="text-gray-500">[{{ $post->created_at }}]</time>
-					<span class="text-gray-700">{{ implode(', ', $post->tags) }}</span>
 				</div>
 				<a href="{{ $post->link() }}" class="text-red-500 hover:underline">{{ $post->title }}</a>
 			</li>
