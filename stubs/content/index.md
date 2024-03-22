de1d20019a355b0c21719ad5f4b1eca5f1138084
bangnokia <bangnokia@gmail.com>
2024-03-22 15:09:15 +0700

    wip

---
+++ stubs/content/index.md
@@ -0,0 +1,8 @@
+---
+title: You blog
+layout: app
+---
+
+# Index page
+
+This is your index page, you can customize it by editing `resources/views/app.blade.php` file.
--- tests/Feature/CreateNewSiteTest.php
+++ tests/Feature/CreateNewSiteTest.php
@@ -3,20 +3,10 @@
 it('can create a skeleton site', function () {
     $this->artisan('new tests/tmp');
 
-    expect('tests/tmp')->and('tests/tmp/posts')->toBeDirectory()
-        ->and(is_link('tests/tmp/public/images'))->toBeTrue()
-        ->and(readlink('tests/tmp/public/images'))->toBe(getcwd().'/tests/tmp/images')
-        ->and(is_link('tests/tmp/public/css'))->toBeTrue()
-        ->and(readlink('tests/tmp/public/css'))->toBe(getcwd().'/tests/tmp/resources/css')
-        ->and(is_link('tests/tmp/public/js'))->toBeTrue()
-        ->and(readlink('tests/tmp/public/js'))->toBe(getcwd().'/tests/tmp/resources/js');
-
-//    $filesystem = new Illuminate\Filesystem\Filesystem();
-
-//    $this->assertEquals(
-//        $filesystem->allFiles(base_path('stubs')),
-//        $filesystem->allFiles(getcwd().'/tests/tmp')
-//    );
+    expect('tests/tmp')
+        ->and('tests/tmp/content/posts')->toBeDirectory()
+        ->and('tests/tmp/content/index.md')->toBeDirectory()
+        ;
 });
 
 it('can not create new site if directory existed', function () {
