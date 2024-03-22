56e907c930583649ffbfe485af6f1af1f9face62
bangnokia <bangnokia@gmail.com>
2024-03-22 19:12:20 +0700

    wip

--- app/MarkdownRenderer.php
+++ app/MarkdownRenderer.php
@@ -6,12 +6,15 @@ use BangNokia\Pekyll\Contracts\Renderer;
 
 class MarkdownRenderer implements Renderer
 {
-    public function __construct(protected string $rootDir)
+    public function __construct(protected string $rootDir, protected Parser $parser)
     {
     }
 
     public function render(string $file): string
     {
-        return file_get_contents($file);
+        $content = file_get_contents($file);
+
+        $result = $this->parser->parse($content);
+        dd($content);
     }
 }
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -5,15 +5,17 @@ namespace BangNokia\Pekyll\Providers;
 use BangNokia\Pekyll\ContentFinder;
 use BangNokia\Pekyll\Contracts\Renderer;
 use BangNokia\Pekyll\Contracts\Router;
+use BangNokia\Pekyll\MarkdownParser;
 use BangNokia\Pekyll\MarkdownRenderer;
+use BangNokia\Pekyll\Parser;
 use Illuminate\Support\ServiceProvider;
 
 class RouteServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->bind(Renderer::class, function () {
-            return new MarkdownRenderer(getcwd() . '/content');
+        $this->app->bind(Renderer::class, function ($app) {
+            return new MarkdownRenderer(getcwd() . '/content', new Parser(new MarkdownParser()));
         });
 
         $this->app->singleton(Router::class, function ($app) {
