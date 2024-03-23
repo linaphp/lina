36c9416702b4387779f1fda5631241947fb03a45
bangnokia <bangnokia@gmail.com>
2024-03-24 03:33:59 +0700

    wip

--- skeleton/resources/views/home.blade.php
+++ skeleton/resources/views/home.blade.php
@@ -1,5 +1,9 @@
 @extends('app')
 
+@php
+    $posts = collect(cf()->index('posts'))->sort(fn($a, $b) => $b->createdAt <=> $a->createdAt);
+@endphp
+
 @section('content')
     <h1 class="text-center">{{ $data->title }}</h1>
 
@@ -9,7 +13,7 @@
     <div>
         <h2>Latest posts</h2>
         <ul style="display: flex; flex-direction: column; gap: 1rem; list-style: none; padding: 0">
-            @foreach(collect(cf()->index('posts'))->reverse() as $post)
+            @foreach($posts as $post)
                 <li>
                     <x-date>{{ $post->createdAt }}</x-date>
                     <a href="{{ $post->url() }}" class="block" style="padding: 0.25rem 0;">{{ $post->title }} {{ $post->createdAt }}</a>
