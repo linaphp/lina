d7fef922b6c2c1eed784e73ec3bdea6e21541379
bangnokia <bangnokia@gmail.com>
2024-04-05 18:26:17 +0700

    wip

--- tests/Feature/CreateNewSiteTest.php
+++ tests/Feature/CreateNewSiteTest.php
@@ -11,7 +11,6 @@ it('can create a skeleton site', function () {
 it('can not create new site if directory existed', function () {
     mkdir('tests/tmp');
 
-
     $this->artisan('new tests/tmp')
         ->expectsConfirmation('The directory tests/tmp already exists. Do you want to continue?', 'no')
         ->assertExitCode(1);
