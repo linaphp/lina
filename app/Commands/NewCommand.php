0182c5fb7aa3307237f0b01e91757c9ce789c5ef
bangnokia <bangnokia@gmail.com>
2024-04-05 18:21:34 +0700

    confirm when creat in an existing folder

--- app/Commands/NewCommand.php
+++ app/Commands/NewCommand.php
@@ -9,18 +9,21 @@ class NewCommand extends Command
 {
     protected $signature = 'new {name}';
 
-    protected $description = 'Create new site';
+    protected $description = 'Create new skeleton';
 
     public function handle(Filesystem $filesystem): int
     {
         $directory = $this->argument('name');
 
-        if ($filesystem->isDirectory($directory)) {
-            $this->error($directory.' already existed');
+        if (
+            $filesystem->isDirectory($directory)
+            && !$this->confirm("The directory $directory already exists. Do you want to continue?", false)
+        ) {
+            $this->info('Aborted!');
             return 1;
         }
 
-        $filesystem->copyDirectory(base_path('skeleton'), $sitePath = getcwd().'/'.$directory);
+        $filesystem->copyDirectory(base_path('skeleton'), $sitePath = getcwd() . '/' . $directory);
 
         $this->info("Your blog scaffolded successfully! 👏");
 
