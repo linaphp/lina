0cb98e84de7a228e026b4daae7acf9e046fb449e
bangnokia <bangnokia@gmail.com>
2024-04-06 02:03:41 +0700

    fix skeleton

--- skeleton/cloudflare-page.sh
+++ skeleton/cloudflare-page.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 curl -sL https://dl.static-php.dev/static-php-cli/common/php-8.3.4-cli-linux-x86_64.tar.gz | tar xz
-curl -o lina https://github.com/bangnokia/lina/releases/latest/download/lina.phar
+curl -sL -o lina.phar https://github.com/bangnokia/lina/releases/latest/download/lina.phar
 
-
-./php lina build
+chmod +x php
+./php lina.phar build
