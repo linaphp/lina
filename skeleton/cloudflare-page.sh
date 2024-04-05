216669a3d45dbd98531ab4213c7eb4547438143f
bangnokia <bangnokia@gmail.com>
2024-04-05 21:20:10 +0700

    update cloudflare build script

--- skeleton/cloudflare-page.sh
+++ skeleton/cloudflare-page.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-## download the binary
 curl -sL https://dl.static-php.dev/static-php-cli/common/php-8.3.4-cli-linux-x86_64.tar.gz | tar xz
-curl -o lina https://raw.githubusercontent.com/bangnokia/lina/main/builds/lina
+curl -o lina https://raw.githubusercontent.com/bangnokia/lina/releases/latest/download/lina.phar
+
 
 ./php lina build
