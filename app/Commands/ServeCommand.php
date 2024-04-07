09e687cbd211b87d32151be383fac15fc0254475
bangnokia <bangnokia@gmail.com>
2024-04-08 02:12:02 +0700

    serve command

---
+++ app/Commands/ServeCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace BangNokia\Lina\Commands;
+
+use Illuminate\Process\Pool;
+use Illuminate\Support\Facades\Process;
+use LaravelZero\Framework\Commands\Command;
+use Symfony\Component\Process\PhpExecutableFinder;
+
+class ServeCommand extends Command
+{
+    protected $signature = 'serve';
+
+    protected $description = 'Start development server';
+
+    public function handle(): int
+    {
+        $phpBinary = (new PhpExecutableFinder())->find();
+        // get the current php binary path which is running the command
+        $pool = Process::pool(function (Pool $pool) use ($phpBinary) {
+            $pool->path(getcwd())->command([$phpBinary, base_path('lina'), 'serve:http']);
+            $pool->path(getcwd())->command([$phpBinary, base_path('lina'), 'serve:ws']);
+        })->start(function (string $type, string $output, string $key) {
+            $this->output->write($output);
+        });
+
+        while ($pool->running()->isNotEmpty()) {
+            usleep(0.5 * 1000000);
+        }
+
+        $pool->wait();
+    }
+}
--- skeleton/content/index.md
+++ skeleton/content/index.md
@@ -3,6 +3,8 @@ title: Welcome to Lina
 layout: home
 ---
 
-Welcome to Lina, a simple and blazing fast blog platform built on top of Laravel Blade. Lina is designed to be easy to use and.
+Welcome to [Lina](https://lina.daudau.cc), a simple and blazing fast blog platform built on top of Laravel Blade.
+
+Lina is designed to be easy to use and.
 
 Checkout the [documentation](https://github.com/bangnokia/lina) on our Github repository to get started.
