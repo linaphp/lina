a939e9179b66580e647c337a3c67d939420b5b48
bangnokia <bangnokia@gmail.com>
2024-05-09 17:06:09 +0700

    wip

--- skeleton/cloudflare-page.sh
+++ skeleton/cloudflare-page.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 curl -sL https://dl.static-php.dev/static-php-cli/common/php-8.3.4-cli-linux-x86_64.tar.gz | tar xz
-curl -sL -o lina.phar https://github.com/bangnokia/lina/releases/latest/download/lina.phar
+curl -sL -o lina.phar https://github.com/linaphp/lina/releases/latest/download/lina.phar
 
 chmod +x php
 ./php lina.phar build
